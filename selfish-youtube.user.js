9608cc846716a52efef8fd13095c55bc8bf3122d
Alex Szczuczko <alex@szc.ca>
2013-05-06 19:21:02 -0400

    Add version to script

--- selfish-youtube.user.js
+++ selfish-youtube.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name                Selfish Youtube
+// @version             2013-05-06
 // @namespace           https://github.com/ASzc/selfish-youtube
 // @description         On the watch page, remove the share panel.
 // @include             http://youtube.com/watch*
